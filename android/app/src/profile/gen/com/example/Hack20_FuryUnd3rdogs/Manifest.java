e1be81cab96daaf537aed06e53a775fac234d473
DhruvSingla007 <dhruvsingla07@gmail.com>
2020-06-29 04:16:19 +0530

    portrait mode emulator added

---
+++ android/app/src/profile/gen/com/example/Hack20_FuryUnd3rdogs/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.Hack20_FuryUnd3rdogs;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
--- lib/widgets/emulator.dart
+++ lib/widgets/emulator.dart
@@ -14,110 +14,119 @@ class _EmulatorState extends State<Emulator> {
 //    SystemChrome.setPreferredOrientations(
 //        [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
     SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: Container(
+    return Scaffold(
+      body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
             fit: BoxFit.cover,
             colorFilter: new ColorFilter.mode(
-                Colors.black.withOpacity(0.2), BlendMode.dstATop),
-            image: AssetImage("assets/images/background.jpg"),
+                Colors.black.withOpacity(0.7), BlendMode.dstATop),
+            image: AssetImage("assets/images/bg1.jpeg"),
           ),
         ),
         padding: EdgeInsets.all(8.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: 50.0,
-                ),
-                Image.asset(
-                  'assets/images/move.png',
-                  height: 50,
-                  width: 50,
-                ),
-                Row(
-                  children: <Widget>[
-                    Image.asset(
-                      'assets/images/move.png',
-                      height: 50,
-                      width: 50,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20.0),
-                    ),
-                    Image.asset(
-                      'assets/images/move.png',
-                      height: 50,
-                      width: 50,
-                    ),
-                  ],
-                ),
-                Image.asset(
-                  'assets/images/move.png',
-                  height: 50,
-                  width: 50,
-                ),
-              ],
-            ),
             Expanded(
+              flex: 2,
               child: Container(
                 decoration: BoxDecoration(
                   color: Colors.black,
                   borderRadius: BorderRadius.circular(30.0),
                 ),
-                child: MinesweeperGame()
+                child: Expanded(child: MinesweeperGame()),
               ),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Row(
+            Expanded(
+              flex: 1,
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.asset(
-                        'assets/images/select.png',
-                        width: 50.0,
-                        height: 50.0,
-                      ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        SizedBox(
+                          height: 50.0,
+                        ),
+                        Image.asset(
+                          'assets/images/move.png',
+                          height: 50,
+                          width: 50,
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Image.asset(
+                              'assets/images/move.png',
+                              height: 50,
+                              width: 50,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 20.0),
+                            ),
+                            Image.asset(
+                              'assets/images/move.png',
+                              height: 50,
+                              width: 50,
+                            ),
+                          ],
+                        ),
+                        Image.asset(
+                          'assets/images/move.png',
+                          height: 50,
+                          width: 50,
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.asset(
-                        'assets/images/select.png',
-                        width: 50.0,
-                        height: 50.0,
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              'assets/images/select.png',
+                              width: 50.0,
+                              height: 50.0,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              'assets/images/select.png',
+                              width: 50.0,
+                              height: 50.0,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
+                      Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              'assets/images/action.png',
+                              height: 50,
+                              width: 50,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              'assets/images/action.png',
+                              height: 50,
+                              width: 50,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                   ],
                 ),
-                Row(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.asset(
-                        'assets/images/action.png',
-                        height: 50,
-                        width: 50,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.asset(
-                        'assets/images/action.png',
-                        height: 50,
-                        width: 50,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+              ),
             ),
           ],
         ),
